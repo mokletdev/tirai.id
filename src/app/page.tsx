abfe4ec288595134e8ba7bfb00f27b96fde5645e
Teguh Bayu Pratama <149038828+teguhbayu@users.noreply.github.com>
2024-11-23 19:25:43 +0700

    feat: Implement google sign in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <h1 className="w-full text-center text-9xl">Testing</h1>;
+  return (
+    <>
+      <h1 className="w-full text-center text-9xl">Testing</h1>
+    </>
+  );
 }
