8c49b1b90f1465d75e5d1445bf27d7b7a358e73b
Ahsan <ahsanaz461@gmail.com>
2024-11-28 17:11:08 +0700

    refactor: Clean up previous front-end code

---
+++ src/app/(use-navbar)/article/components/MostRead.tsx
@@ -0,0 +1,10 @@
+import { SectionContainer } from "@/components/layout/SectionContainer";
+import { H1 } from "@/components/ui/text";
+
+export const MostRead = () => {
+  return (
+    <SectionContainer>
+      <H1 className="mb-[1.375rem] text-black">Artikel Lainnya</H1>
+    </SectionContainer>
+  );
+};
