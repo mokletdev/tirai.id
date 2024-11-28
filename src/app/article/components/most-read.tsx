5988704d0a10b0778df60cf65aecdf5067b67305
Devdit <pradita.cleine@gmail.com>
2024-11-28 15:43:43 +0700

    feat: add most read section in article page

---
+++ src/app/article/components/most-read.tsx
@@ -0,0 +1,11 @@
+import { PageContainer } from "@/components/layout/PageContainer";
+import { SectionContainer } from "@/components/layout/SectionContainer";
+import { H1, H2, H3, H4, H5, H6 } from "@/components/ui/text";
+import { SectionTitle } from "@/components/widget/SectionTitle";
+export default function MostRead() {
+  return (
+    <SectionContainer>
+      <H1 className="mb-[1.375rem] text-black">Paling Banyak Dibaca</H1>
+    </SectionContainer>
+  );
+}
