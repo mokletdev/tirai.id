654637add51036e53a6ca77902c1b90445c8a260
Teguh Bayu Pratama <149038828+teguhbayu@users.noreply.github.com>
2024-11-29 08:52:52 +0700

    feat: Implement SEO for view article page

--- src/app/(use-navbar)/article/[slug]/page.tsx
+++ src/app/(use-navbar)/article/[slug]/page.tsx
@@ -1,5 +1,39 @@
 import { PageContainer } from "@/components/layout/PageContainer";
 import { findArticle } from "@/utils/database/article.query";
+import type { Metadata, ResolvingMetadata } from "next";
+
+type Props = {
+  params: Promise<{ slug: string }>;
+};
+
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata,
+): Promise<Metadata> {
+  const slug = (await params).slug;
+
+  const article = await findArticle({ slug });
+
+  const previousImages = (await parent).openGraph?.images || [];
+
+  return {
+    title: `${article?.title} - ${article?.author.name}`,
+    openGraph: {
+      images: [article?.cover_url!, ...previousImages],
+    },
+    authors: [
+      {
+        name: article?.author.name,
+      },
+    ],
+    description: article?.description,
+    keywords: article?.tags,
+    robots: {
+      index: true,
+      nocache: false,
+    },
+  };
+}
 
 export default async function ArticlePage({
   params,
