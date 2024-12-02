f4021c196782437d1e8c706b6150c11ee9308c7c
Ahsan <ahsanaz461@gmail.com>
2024-12-02 13:03:43 +0700

    refactor: Move /about-us to /about

--- src/app/(use-navbar)/about/components/Hero.tsx
+++ src/app/(use-navbar)/about/components/Hero.tsx
@@ -1,15 +1,12 @@
-import Image from "next/image";
 import { SectionContainer } from "@/components/layout/SectionContainer";
-import { H2, Body1 } from "@/components/ui/text";
+import { Body3, H2 } from "@/components/ui/text";
 import { SectionTitle } from "@/components/widget/SectionTitle";
-import { ArrowRight } from "lucide-react";
-import Link from "next/link";
-import { buttonVariants } from "@/components/ui/button";
+import Image from "next/image";
 
 export default function Hero() {
   return (
-    <SectionContainer id="Hero">
-      <div className="flex flex-col justify-between space-y-[3.75rem] lg:flex-row lg:space-y-0">
+    <SectionContainer id="hero">
+      <div className="flex flex-col items-end justify-between space-y-[3.75rem] lg:flex-row lg:space-y-0">
         <Image
           src={"/assets/login.png"}
           alt="Hero Image"
@@ -17,18 +14,15 @@ export default function Hero() {
           height={648}
           className="pointer-events-none"
         />
-        <div
-          id="paragraph"
-          className="flex w-full flex-col justify-between space-y-[3.75rem] lg:max-w-[50%] lg:space-y-0"
-        >
-          <div className="flex flex-col">
+        <div className="flex w-full flex-col lg:max-w-[50%]">
+          <div className="mb-16 flex flex-col">
             <SectionTitle>Sejarah</SectionTitle>
             <H2 className="text-black">
               Perjalanan Kami Menuju Standar Terbaik dalam Kualitas dan Desain
               Tirai
             </H2>
           </div>
-          <Body1 className="text-black opacity-50">
+          <Body3 className="text-neutral-500">
             Tirai.id memulai perjalanan pada pertengahan 2009 sebagai usaha
             kecil di kawasan perumahan, melayani kebutuhan tirai rumah tangga
             dengan fokus pada kualitas. Pada tahun 2010, kami menjalin kerja
@@ -36,18 +30,19 @@ export default function Hero() {
             memperkenalkan produk berkualitas kepada lebih banyak pelanggan.
             Hingga kini, kami terus berinovasi untuk menghadirkan tirai yang
             estetis, fungsional, dan sesuai dengan kebutuhan pelanggan.
-          </Body1>
-          <div className="flex flex-row justify-start">
-            <Link
-              href={"#"}
-              className={buttonVariants({
-                variant: "default",
-                className: "w-[212px] lg:w-fit",
-              })}
-            >
-              Baca Selengkapnya <ArrowRight />
-            </Link>
-          </div>
+            <br />
+            <br />
+            Kami menyediakan ready stock gordyn dalam kemasan yang praktis dan
+            siap pakai. Produk kami telah tersedia di berbagai pasar modern
+            ternama seperti Giant dan Marco, serta telah menjadi bagian dari
+            jaringan pemasaran multilevel marketing (MLM) Sophie Paris (Sophie
+            Martin). Dengan kualitas yang terjamin dan desain yang elegan, kami
+            menyasar segmen pasar kelas menengah modern minimalis yang
+            mengutamakan kepraktisan dan gaya. Namun, produk kami juga dapat
+            memenuhi kebutuhan kalangan atas dengan preferensi desain klasik,
+            sehingga mencakup berbagai lapisan masyarakat dengan selera yang
+            beragam.
+          </Body3>
         </div>
       </div>
     </SectionContainer>
